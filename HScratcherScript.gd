f8cb3330fb3dae58e279a20cd9c224d9937878ef
HenryPedersen <henry.pedersen5847@gmail.com>
2026-03-04 16:53:59 -0600

    Added instantiation to Henry Table, planning for future code

--- HScratcherScript.gd
+++ HScratcherScript.gd
@@ -1,8 +1,13 @@
 extends Node2D
 
+#TODO Change mourse drag so that it doesn't affect other Scratchers
+#TODO Make collision between Scratchers push them away with icy
+#physics. Check with group on push/overlap
+
 var mouseEntered : bool
 var mousePos : Vector2 = Vector2.ZERO
 var difference : Vector2
+var rng = RandomNumberGenerator.new()
 
 func _on_area_2d_mouse_entered() -> void:
 	mouseEntered = true
@@ -17,3 +22,8 @@ func _process(delta: float) -> void:
 		global_position -= difference
 
 	mousePos = get_global_mouse_position()
+
+func _ready() -> void:
+	var randX = rng.randf_range(480, 1920)
+	var randY = rng.randf_range(0, 1080)
+	position = Vector2(randX, randY)
