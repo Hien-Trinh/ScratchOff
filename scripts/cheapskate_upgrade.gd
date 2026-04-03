b7d93c7a06e67582912035fad6980c618c84b76b
HenryPedersen <henry.pedersen5847@gmail.com>
2026-04-03 10:40:25 -0500

    upgrade progress

--- scripts/cheapskate_upgrade.gd
+++ scripts/cheapskate_upgrade.gd
@@ -1,8 +1,6 @@
 extends Upgrade # Subclass of the Upgrade class
 
-#shop prices are reduced 25%
-
-#reference to shopmenu? Not sure how that works with scripts
+#shop prices are reduced 10% - 25% for each item
 
 func _init(new_name : String, new_texture : Texture2D):
 	upgrade_name = new_name
@@ -11,7 +9,7 @@ func _init(new_name : String, new_texture : Texture2D):
 #this function is called when entering shop, prices set, and this upgrade active
 func cheapskate():
 	pass #for now
-	#$Tickets/Ticket1/Buy_1.buy_cost = tick1[2]
-	#$Tickets/Ticket1/Buy_2.buy_cost = tick2[2]
-	#$Tickets/Ticket1/Buy_3.buy_cost = tick3[2]
-	#$Tickets/Ticket1/Buy_4.buy_cost = tick4[2]
+	#$Tickets/Ticket1/Buy_1.buy_cost = tick1[2] * randf_range(0.1,0.25)
+	#$Tickets/Ticket1/Buy_2.buy_cost = tick2[2] * randf_range(0.1,0.25)
+	#$Tickets/Ticket1/Buy_3.buy_cost = tick3[2] * randf_range(0.1,0.25)
+	#$Tickets/Ticket1/Buy_4.buy_cost = tick4[2] * randf_range(0.1,0.25)
