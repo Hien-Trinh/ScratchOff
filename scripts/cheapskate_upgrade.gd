7ccee07755632c4181ca2a2916e2e18fcef17ffe
HenryPedersen <henry.pedersen5847@gmail.com>
2026-04-03 04:07:05 -0500

    back on upgrades, deleted speedrun added cheapskate

--- scripts/cheapskate_upgrade.gd
+++ scripts/cheapskate_upgrade.gd
@@ -2,11 +2,16 @@ extends Upgrade # Subclass of the Upgrade class
 
 #shop prices are reduced 25%
 
+#reference to shopmenu? Not sure how that works with scripts
+
 func _init(new_name : String, new_texture : Texture2D):
 	upgrade_name = new_name
 	upgrade_texture = new_texture
 
-#this function is called when entering shop and this upgrade active
+#this function is called when entering shop, prices set, and this upgrade active
 func cheapskate():
 	pass #for now
-#	Shop isn't 
+	#$Tickets/Ticket1/Buy_1.buy_cost = tick1[2]
+	#$Tickets/Ticket1/Buy_2.buy_cost = tick2[2]
+	#$Tickets/Ticket1/Buy_3.buy_cost = tick3[2]
+	#$Tickets/Ticket1/Buy_4.buy_cost = tick4[2]
