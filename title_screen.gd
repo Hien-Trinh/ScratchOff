a219cef2197827100394e72eddd5d51181edb61d
Samuel Kennedy <sam.kndy16@gmail.com>
2026-04-07 20:48:13 -0500

    Functional title screen

---
+++ title_screen.gd
@@ -0,0 +1,20 @@
+extends Control
+
+@onready var start_button = $TextureRect2/StartButton
+@onready var credits_button = $TextureRect2/StartButton
+@onready var credits_window = $CreditsWindow
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	credits_window.visible = false
+
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file("res://game_view.tscn")
+
+func _on_exit_credit_window_pressed():
+	if(credits_window.visible == true):
+		credits_window.visible = false
+
+func _on_credits_button_pressed():
+	if(credits_window.visible == false):
+		credits_window.visible = true
