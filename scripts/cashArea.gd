d9db393bc842aa9f221d4cee4e4681b8329b4ee5
HenryPedersen <henry.pedersen5847@gmail.com>
2026-04-17 11:04:21 -0500

    explosion is back

--- scripts/cashArea.gd
+++ scripts/cashArea.gd
@@ -48,7 +48,7 @@ func _process(delta: float) -> void:
 				card.queue_free()
 				anim_explo.play("default")
 				# Await animation finish
-				anim_explo.animation_finished.connect(spawned_explosion_sprite.queue_free)
+				await anim_explo.animation_finished
 				# Remove explosion sprite from tree
 				spawned_explosion_sprite.queue_free()
 				# Can cause bug: attempt to call queue free on a previously freed null instance
